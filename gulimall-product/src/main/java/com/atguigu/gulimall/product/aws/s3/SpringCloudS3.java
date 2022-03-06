885ccc4841d6872338eeac8c8bb98e179a364150
wangran326990 <wangrancanada@gmail.com>
2022-03-06 16:57:06 -0500

    integrated with aws

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/aws/s3/SpringCloudS3.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.product.aws.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.core.io.s3.PathMatchingSimpleStorageResourcePatternResolver;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+@Component
+public class SpringCloudS3 {
+    @Autowired
+    private AmazonS3Client s3Client;
+}
